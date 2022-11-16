9db723cbdfa728a77e1db92488e71fb023e83400
Karolina Jelen <jehonala@gmail.com>
2022-11-16 09:21:02 +0100

    Simplify color gradient and add type hints

--- colors.py
+++ colors.py
@@ -1,22 +1,34 @@
-def get_key_color(gradient, key):
+from __future__ import annotations
+
+RGBColor = tuple[int, int, int]
+NoteName = str
+Octave = int
+Note = tuple[NoteName, Octave]
+
+# TODO what to do if gradient color not found?
+def get_key_color(gradient: list(RGBColor), key: Note) -> RGBColor:
     return gradient[key]
 
 
 # TODO make a better gradient - maybe rainbow?
-def linear_gradient(start_color, end_color, n):
+def linear_gradient(
+    start_color: RGBColor, end_color: RGBColor, n: int
+) -> list(RGBColor):
     """
     Returns a gradient list of n colors between RGB colors.
     """
-    # Initilize a list of the output colors with the starting color
-    gradient_list = [start_color]
-    # Calculate a color at each evenly spaced value of t from 1 to n
-    for t in range(1, n):
-        # Interpolate RGB vector for color at the current value of t
-        curr_vector = [
-            int(start_color[j] + (float(t) / (n - 1)) * (end_color[j] - start_color[j]))
-            for j in range(3)
-        ]
-        # Add it to our list of output colors
-        gradient_list.append(curr_vector)
 
-    return gradient_list
+    def next_color_value(start_color_value, end_color_value, step, n):
+        return int(
+            start_color_value +
+            step / (n - 1) * (end_color_value - start_color_value)
+            )
+
+    gradient = []
+    for i in range(0, n):
+        r = next_color_value(start_color[0], end_color[0], i, n)
+        g = next_color_value(start_color[1], end_color[1], i, n)
+        b = next_color_value(start_color[2], end_color[2], i, n)
+        gradient.append((r, g, b))
+
+    return gradient
